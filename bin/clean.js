c7d6254c0be79dba8bed67933fe676b9133a8653
Alboaie Sînică <salboaie@gmail.com>
2020-11-20 15:11:48 +0200

    basic clean script
    To be integrated in octopus

---
+++ bin/clean.js
@@ -0,0 +1,84 @@
+let myPath = process.argv[2];
+
+let fs = require('fs');
+let path = require('path');
+
+function  walk(dir, filterFiles, filterFolders, done) {
+    let results = [];
+
+    let recWalk = function(dir, filterFiles, filterFolders, done) {
+        fs.readdir(dir, function(err, list) {
+            if (err) return done(err, result);
+            let i = 0;
+            (function next() {
+                let file = list[i++];
+                if (!file) return done(null, results);
+                file = path.resolve(dir, file);
+                fs.stat(file, function(err, stat) {
+                    if (stat && stat.isDirectory()) {
+                        if(filterFolders(file)) {
+                            results.push(file);
+                        }
+                        recWalk(file, filterFiles, filterFolders,function(err, res) {
+                            next();
+                        });
+                    } else {
+                        if(filterFiles(file)) {
+                            results.push(file);
+                        }
+                        next();
+                    }
+                });
+            })();
+        });
+    };
+
+    recWalk(dir, filterFiles, filterFolders, done);
+}
+
+
+
+
+function filterFiles(name){
+
+    if(name.endsWith("/seed") ){
+            console.log("Deleting seed file:", name);
+            fs.unlinkSync(name);
+        }
+    return undefined;
+}
+
+
+function deleteFolderContent(name, text){
+    walk(name,
+        function(name){
+            console.log(text, name);
+            fs.unlinkSync(name);
+        },
+        function(name){
+        },
+        function(err, result){}
+        );
+}
+
+function filterFolders(name){
+    if(name){
+        if(name.endsWith("/anchors") >0 ){
+            deleteFolderContent(name, "Deleting anchors:")
+
+        }
+        if(name.indexOf("/brick-storage/") >0){
+            deleteFolderContent(name, "Deleting bricks:")
+        }
+    }
+    return undefined;
+}
+
+walk(myPath, filterFiles, filterFolders, function(err, result){
+    result.map( name => {
+        console.log("Deleting folder:", name);
+        fs.unlinkSync(name);
+    })
+
+});
+
