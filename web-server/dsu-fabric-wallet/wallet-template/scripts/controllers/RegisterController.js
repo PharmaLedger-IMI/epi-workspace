d5d1d544e59246db54707dd887afd54c8b374faf
skutner <sava.dumitru.daniel@gmail.com>
2020-11-22 20:00:58 +0200

    configured dsu-fabric-wallet

---
+++ web-server/dsu-fabric-wallet/wallet-template/scripts/controllers/RegisterController.js
@@ -0,0 +1,8 @@
+import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
+
+export default class titleController extends ContainerController {
+    constructor(element, history) {
+        super(element);
+        this.setModel({});
+    }
+}
