ae13da1b44e2fd6df50d5b668c070b88c0c02d1e
salboaie <salboaie@gmail.com>
2020-11-30 11:28:24 +0200

    add support for generating the holder

---
+++ web-server/dsu-fabric-wallet/wallet-template/scripts/controllers/HolderDomainController.js
@@ -0,0 +1,52 @@
+import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
+import constants from "./constants.js";
+
+export default class HolderDomainController extends ContainerController {
+    constructor(element, history) {
+        super(element, history);
+
+        this.setModel({});
+
+        this.on('openFeedback', (e) => {
+            this.feedbackEmitter = e.detail;
+        });
+
+        this.on("generate-identity", (event) => {
+            const opendsu = require("opendsu");
+            const keyssiSpace = opendsu.loadApi("keyssi");
+            const seedSSI = keyssiSpace.buildSeedSSI(this.model.domain);
+            this.DSUStorage.getObject(constants.HOLDER_FILE_PATH, (err, holder)=>{
+                if(err){
+                    holder = {};
+                }
+
+                holder.domain = this.model.domain;
+                holder.ssi = seedSSI.getIdentifier();
+                this.DSUStorage.setObject(constants.HOLDER_FILE_PATH, holder, (err)=>{
+                    if(err){
+                        return this.showError(err);
+                    }
+                    history.push({
+                        pathname: '/holder'
+                    });
+                });
+            });
+
+        });
+    }
+
+    showError(err, title, type) {
+        let errMessage;
+        title = title ? title : 'Validation Error';
+        type = type ? type : 'alert-danger';
+
+        if (err instanceof Error) {
+            errMessage = err.message;
+        } else if (typeof err === 'object') {
+            errMessage = err.toString();
+        } else {
+            errMessage = err;
+        }
+        this.feedbackEmitter(errMessage, title, type);
+    }
+}
--- web-server/dsu-fabric-wallet/wallet-template/scripts/controllers/constants.js
+++ web-server/dsu-fabric-wallet/wallet-template/scripts/controllers/constants.js
@@ -1,4 +1,6 @@
 const C = {
-	"ISSUER_FILE_PATH" : "/myKeys/issuer.json"
+	"ISSUER_FILE_PATH" : "/myKeys/issuer.json",
+	"HOLDER_FILE_PATH" : "/myKeys/holder.json",
+	"CREDENTIAL_FILE_PATH" : "/myKeys/credential.json"
 };
 export default C;
