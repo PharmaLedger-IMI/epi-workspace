eb0c04f3c0acfb71c2f0b5141ce41c9ee0428166
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-12-04 21:33:14 +0200

    Added sw config to trust-loader-config environments.

--- trust-loader-config/dsu-fabric-wallet/loader/environment.js
+++ trust-loader-config/dsu-fabric-wallet/loader/environment.js
@@ -6,5 +6,6 @@ export default {
   "browser":  "any",
   "mode":  "dev-secure",
   "domain":  "vault",
-  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure)"
+  "sw": true,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false)"
 }
--- trust-loader-config/leaflet-wallet/loader/environment.js
+++ trust-loader-config/leaflet-wallet/loader/environment.js
@@ -6,5 +6,6 @@ export default  {
   "browser":  "any",
   "mode":  "autologin",
   "domain":  "vault",
-  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release)"
+  "sw": true,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false)"
 }
