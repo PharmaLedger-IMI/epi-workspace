3f53dfaf84851c0ed8afb39865c828705ff15327
Sinica <salboaie@gmail.com>
2020-12-22 15:33:27 +0200

    apihub-root content

---
+++ apihub-root/dsu-fabric-wallet/wallet-patch/scripts/controllers/IssuerController.js
@@ -0,0 +1,42 @@
+import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
+import constants from "./constants.js";
+
+export default class IssuerController extends ContainerController {
+    constructor(element, history) {
+        super(element, history);
+
+        this.setModel({displayCredential: false});
+        this.model.domain = "epi";
+
+        this.DSUStorage.getObject(constants.ISSUER_FILE_PATH, (err, issuer)=>{
+            if(err){
+                return this.History.navigateToPageByTag("issuer-enter-domain");
+            }
+
+            this.model.issuer = issuer;
+            const keyssi = require("opendsu").loadApi("keyssi");
+            let seedSSI = keyssi.parse(issuer.ssi);
+            this.model.issuerPublicIdentity = seedSSI.derive().getIdentifier();
+            this.model.title = `Copy/paste the identifier of a new user in domain [${issuer.domain}]`;
+        });
+
+        this.on('openFeedback', (e) => {
+            this.feedbackEmitter = e.detail;
+        });
+
+        this.on("generate-credential", (event)=>{
+            let userIdentity = this.model.userIdentity;
+            let opendsu = require("opendsu");
+            let crypto = opendsu.loadApi("crypto");
+
+            crypto.createCredential(this.model.issuer.ssi, userIdentity, (err, credential)=>{
+                if(err){
+                    return this.showError(err, "Failed to create credential.");
+                }
+                this.model.credential = credential;
+                this.model.displayCredential = true;
+            });
+
+        }, {capture: true});
+    }
+}
