e45bed591621a78aec38651362b0b706174c0d22
Costin Andronache <Costin.Andronache@endava.com>
2020-12-28 13:33:32 +0200

    Update paths for PSKNodeServer after rename

---
+++ mobile/config/scan-app/ios/PSKNodeServer/PSKNodeServer/nodejsProject/apihub-root/app/loader/environment.js
@@ -0,0 +1,12 @@
+export default  {
+  "appName": "eLeaflet",
+  "vault": "server",
+  "agent": "browser",
+  "system":   "any",
+  "browser":  "any",
+  "mode":  "autologin",
+  "domain":  "vault",
+  "sw": false,
+  "pwa": false,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false)"
+}
