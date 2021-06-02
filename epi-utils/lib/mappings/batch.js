bb512afe80e4d6da00107b54e1f44444158d451d
adrian.ganga <adrian.ganga@heavensolutions.com>
2021-06-02 13:30:53 +0300

    Add batch import and other small fixes

--- epi-utils/lib/mappings/batch.js
+++ epi-utils/lib/mappings/batch.js
@@ -1,8 +1,144 @@
-function verifyIfBatchMessage(message){
-	return message.messageType === "Batch" && typeof message.batch === "object";
+const gtinResolver = require("gtin-resolver");
+
+function verifyIfBatchMessage(message) {
+  return message.messageType === "Batch" && typeof message.batch === "object";
 }
 
-function processBatchMessage(message){
+async function processBatchMessage(message) {
+  const constants = require("./utils").constants;
+  const getBloomFilterSerialisation = require("./utils").getBloomFilterSerialisation;
+  const propertiesMapping = require("./utils").batchDataSourceMapping;
+  const convertDateTOGMTFormat = require("./utils").convertDateTOGMTFormat;
+  const mappingLogService = require("./logs").createInstance(this.storageService);
+
+  const batchId = message.batch.batch;
+  const productCode = message.batch.productCode;
+  const expiryDate = message.batch.expiryDate;
+
+
+  const gtinSSI = gtinResolver.createGTIN_SSI(this.options.holderInfo.domain, this.options.holderInfo.subdomain, productCode);
+  const {dsu: prodDSU, alreadyExists: prodExists} = await this.loadConstSSIDSU(gtinSSI);
+
+  if (!prodExists) {
+    throw new Error("Fail to create a batch for a missing product");
+  }
+
+  const gtinBatchSSI = gtinResolver.createGTIN_SSI(this.options.holderInfo.domain, this.options.holderInfo.subdomain, productCode, batchId, expiryDate);
+  const {dsu: batchConstDSU, alreadyExists: batchExists} = await this.loadConstSSIDSU(gtinBatchSSI);
+
+  let batchDSU;
+  let batchMetadata = {};
+  let latestProductMetadata = {};
+  if (!batchExists) {
+    batchDSU = await this.createDSU(this.options.holderInfo.subdomain, "seed");
+  } else {
+    try {
+      latestProductMetadata = await this.storageService.getRecord(constants.LAST_VERSION_PRODUCTS_TABLE, productCode);
+      if (latestProductMetadata.version < message.batch.epiLeafletVersion) {
+        throw new Error("Fail to create a batch for a missing product version");
+      }
+      batchMetadata = await this.storageService.getRecord(constants.BATCHES_STORAGE_TABLE, batchId);
+    } catch (e) {
+      //ignore
+    }
+    batchDSU = await this.loadDSU(batchMetadata.keySSI);
+  }
+
+
+  const indication = {batch: constants.BATCH_STORAGE_FILE};
+
+  await this.loadJSONS(batchDSU, indication);
+
+  if (typeof this.batch === "undefined") {
+    this.batch = JSON.parse(JSON.stringify(batchMetadata));
+  }
+
+  let productRecord;
+  try {
+    productRecord = await this.storageService.getRecord(constants.PRODUCTS_TABLE, `${productCode}|${message.batch.epiLeafletVersion}`);
+  } catch (e) {
+  }
+
+  for (let prop in propertiesMapping) {
+    this.batch[prop] = message.batch[propertiesMapping[prop]];
+  }
+  if (message.batch.expiryDate) {
+    try {
+      const y = message.batch.expiryDate.slice(0, 2);
+      const m = message.batch.expiryDate.slice(2, 4);
+      const d = message.batch.expiryDate.slice(4, 6);
+      const localDate = new Date(Date.parse(m + '/' + d + '/' + y));
+      const gmtDate = new Date(localDate.getFullYear() + '-' + m + '-' + d + 'T00:00:00Z');
+      this.batch.expiryForDisplay = gmtDate.getTime();
+    } catch (e) {
+      throw new Error(`${message.batch.expiryDate} date is invalid`, e);
+    }
+  }
+
+  this.batch.productName = productRecord.name
+  this.batch.productDescription = productRecord.description;
+  this.batch.creationTime = convertDateTOGMTFormat(new Date());
+  this.batch.msessageTime = message.messageDateTime;
+
+  if (!batchExists) {
+    this.batch.bloomFilterSerialisations = [];
+    this.batch.bloomFilterRecalledSerialisations = [];
+    this.batch.bloomFilterDecommissionedSerialisations = [];
+  }
+
+  let bf;
+  if (message.batch.snValid.length > 0) {
+    bf = getBloomFilterSerialisation(message.batch.snValid);
+    this.batch.bloomFilterSerialisations.push(bf.bloomFilterSerialisation());
+  }
+
+  if (message.batch.snRecalled.length > 0) {
+    bf = getBloomFilterSerialisation(message.batch.snRecalled);
+    this.batch.bloomFilterRecalledSerialisations.push(bf.bloomFilterSerialisation());
+  }
+  if (message.batch.snDecom.length > 0) {
+    bf = getBloomFilterSerialisation(message.batch.snDecom);
+    this.batch.bloomFilterDecommissionedSerialisations.push(bf.bloomFilterSerialisation());
+  }
+
+  const batchClone = JSON.parse(JSON.stringify(this.batch));
+
+  delete this.batch.serialNumbers;
+  delete this.batch.recalledSerialNumbers;
+  delete this.batch.decommissionedSerialNumbers;
+
+  await this.saveJSONS(batchDSU, indication);
+
+
+  if (!batchExists) {
+    await batchConstDSU.mount(constants.BATCH_DSU_MOUNT_POINT, batchDSU);
+  }
+
+  this.batch.keySSI = await batchDSU.getKeySSIAsString();
+  batchClone.keySSI = this.batch.keySSI;
+
+  if (typeof this.options.logService !== "undefined") {
+    await $$.promisify(this.options.logService.log.bind(this.options.logService))({
+      logInfo: this.batch,
+      username: message.senderId,
+      action: batchExists ? "Edited batch" : "Created batch",
+      logType: 'BATCH_LOG'
+    });
+  }
+
+  let batchRecord;
+  try {
+    batchRecord = await this.storageService.getRecord(constants.BATCHES_STORAGE_TABLE, this.batch.batchNumber);
+  } catch (e) {
+  }
+
+  if (!batchRecord) {
+    await this.storageService.insertRecord(constants.BATCHES_STORAGE_TABLE, this.batch.batchNumber, batchClone);
+  } else {
+    await this.storageService.updateRecord(constants.BATCHES_STORAGE_TABLE, this.batch.batchNumber, batchClone);
+  }
+
+  await mappingLogService.logMapping(message, batchId, batchExists ? "updated" : "created", "success");
 
 }
 
--- epi-utils/lib/mappings/utils.js
+++ epi-utils/lib/mappings/utils.js
@@ -1,43 +1,119 @@
 module.exports.productDataSourceMapping = {
-    name : "nameMedicinalProduct",
-    gtin : "productCode",
-    photo : "",
-    description : "",
-    leaflet : "",
-    manufName : "",
-    batchSpecificVersion : false,
-    reportURL : "adverseEventReportingURL",
-    antiCounterfeitingURL : "acfProductCheckURL",
-    adverseEventsReportingEnabled : "flagEnableAdverseEventReporting",
-    antiCounterfeitingEnabled : "flagEnableACFProductCheck",
-    showEPIOnBatchRecalled : "flagDisplayEPI_BatchRecalled",
-    showEPIOnSNRecalled : "flagDisplayEPI_SNRecalled",
-    showEPIOnSNDecommissioned : "flagDisplayEPI_SNDecommissioned",
-    showEPIOnSNUnknown : "flagDisplayEPI_SNUnknown",
-    showEPIOnIncorrectExpiryDate : "flagDisplayEPI_EXPIncorrect",
-    showEPIOnBatchExpired : "flagDisplayEPI_BatchExpired",
-    practitionerInfo : "healthcarePractitionerInfo",
-    patientLeafletInfo : "patientSpecificLeaflet",
-    markets:"markets",
-    internalMaterialCode:"internalMaterialCode",
-    inventedName:"inventedName",
-    strength:"strength"
+  name: "nameMedicinalProduct",
+  gtin: "productCode",
+  photo: "",
+  description: "",
+  leaflet: "",
+  manufName: "",
+  batchSpecificVersion: false,
+  reportURL: "adverseEventReportingURL",
+  antiCounterfeitingURL: "acfProductCheckURL",
+  adverseEventsReportingEnabled: "flagEnableAdverseEventReporting",
+  antiCounterfeitingEnabled: "flagEnableACFProductCheck",
+  showEPIOnBatchRecalled: "flagDisplayEPI_BatchRecalled",
+  showEPIOnSNRecalled: "flagDisplayEPI_SNRecalled",
+  showEPIOnSNDecommissioned: "flagDisplayEPI_SNDecommissioned",
+  showEPIOnSNUnknown: "flagDisplayEPI_SNUnknown",
+  showEPIOnIncorrectExpiryDate: "flagDisplayEPI_EXPIncorrect",
+  showEPIOnBatchExpired: "flagDisplayEPI_BatchExpired",
+  practitionerInfo: "healthcarePractitionerInfo",
+  patientLeafletInfo: "patientSpecificLeaflet",
+  markets: "markets",
+  internalMaterialCode: "internalMaterialCode",
+  inventedName: "inventedName",
+  strength: "strength"
+};
+module.exports.batchDataSourceMapping = {
+  gtin: "productCode",
+  batchNumber: "batch",
+  expiryForDisplay: "expiryDate",
+  version: "epiLeafletVersion",
+  versionLabel: "",
+  serialNumbers: "snValid",
+  recalledSerialNumbers: "snRecalled",
+  decommissionedSerialNumbers: "snDecom",
+  defaultSerialNumber: "",
+  decommissionReason: "",
+  recalled: "flagEnableBatchRecallMessage",
+  serialCheck: "flagEnableSNVerification",
+  incorrectDateCheck: "flagEnableEXPVerification",
+  expiredDateCheck: "flagEnableExpiredEXPCheck",
+  recalledMessage: "recallMessage",
+  defaultMessage: "batchMessage",
+  packagingSiteName: "packagingSiteName",
+  flagEnableACFBatchCheck: "flagEnableACFBatchCheck",
+  acfBatchCheckURL: "acfBatchCheckURL"
 };
 
 module.exports.constants = {
-    'PACKAGES_STORAGE_PATH': "/app/data/packages.json",
-        'DATA_STORAGE_PATH': "/app/data",
-        'PRODUCTS_TABLE': "products",
-        'LAST_VERSION_PRODUCTS_TABLE': "last_version_products",
-        'LOGS_TABLE': "logs",
-        'SERIAL_NUMBERS_LOGS_TABLE': "serial_numbers_logs",
-        'PRODUCT_KEYSSI_STORAGE_TABLE': "productKeySSIs",
-        'BATCHES_STORAGE_TABLE': "batches",
-        'PRODUCT_DSU_MOUNT_POINT': "/gtinDSU",
-        'BATCH_STORAGE_FILE': "/batch.json",
-        'PRODUCT_STORAGE_FILE': "/product.json",
-        'PRODUCT_IMAGE_FILE': "/image.png",
-        'LEAFLET_ATTACHMENT_FILE': "/leaflet.xml",
-        'SMPC_ATTACHMENT_FILE': "/smpc.xml",
-        'IMPORT_LOGS':'import-logs'
+  'PACKAGES_STORAGE_PATH': "/app/data/packages.json",
+  'DATA_STORAGE_PATH': "/app/data",
+  'PRODUCTS_TABLE': "products",
+  'LAST_VERSION_PRODUCTS_TABLE': "last_version_products",
+  'LOGS_TABLE': "logs",
+  'SERIAL_NUMBERS_LOGS_TABLE': "serial_numbers_logs",
+  'PRODUCT_KEYSSI_STORAGE_TABLE': "productKeySSIs",
+  'BATCHES_STORAGE_TABLE': "batches",
+  'PRODUCT_DSU_MOUNT_POINT': "/gtinDSU",
+  'BATCH_DSU_MOUNT_POINT': "/batch",
+  'BATCH_STORAGE_FILE': "/batch.json",
+  'PRODUCT_STORAGE_FILE': "/product.json",
+  'PRODUCT_IMAGE_FILE': "/image.png",
+  'LEAFLET_ATTACHMENT_FILE': "/leaflet.xml",
+  'SMPC_ATTACHMENT_FILE': "/smpc.xml",
+  'IMPORT_LOGS': 'import-logs'
 };
+
+module.exports.getBloomFilterSerialisation = function (arr, bfSerialisation) {
+  let crypto = require("opendsu").loadAPI("crypto");
+  let bf;
+  if (bfSerialisation) {
+    bf = crypto.createBloomFilter(bfSerialisation);
+  } else {
+    bf = crypto.createBloomFilter({estimatedElementCount: arr.length, falsePositiveTolerance: 0.000001});
+  }
+  arr.forEach(sn => {
+    bf.insert(sn);
+  });
+  return bf
+}
+
+module.exports.convertDateTOGMTFormat = function (date) {
+  let formatter = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false,
+    weekday: "short",
+    monthday: "short",
+    timeZone: 'GMT'
+  });
+
+  let arr = formatter.formatToParts(date);
+  let no = {};
+  arr.forEach(item => {
+    no[item.type] = item.value;
+  })
+  let {year, month, day, hour, minute} = no;
+
+  let offset = -date.getTimezoneOffset();
+  let offset_min = offset % 60;
+  if (!offset_min) {
+    offset_min = "00"
+  }
+  offset = offset / 60;
+  let offsetStr = "GMT ";
+  if (offset) {
+    if (offset > 0) {
+      offsetStr += "+";
+    }
+    offsetStr += offset;
+    offsetStr += ":";
+    offsetStr += offset_min;
+  }
+
+  return `${year} ${month} ${day} ${hour}:${minute} ${offsetStr}`;
+}
