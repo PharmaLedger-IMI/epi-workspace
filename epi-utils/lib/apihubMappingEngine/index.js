00b832590303e81d83d56822669c2b3ad8fc7da3
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2021-06-13 16:07:17 +0300

    fixed workspace fresh build and server start

--- epi-utils/lib/apihubMappingEngine/index.js
+++ epi-utils/lib/apihubMappingEngine/index.js
@@ -1,39 +1,49 @@
 function getEPIMappingEngineForAPIHUB(server) {
+    let mappingEngineInitialized = false;
 
-    const gtinResolverBundle = "./../../../gtin-resolver/build/bundles/gtinResolver.js";
-    require(gtinResolverBundle);
-    const epiUtilsBundle = "./../../../epi-utils/build/bundles/epiUtils.js";
-    require(epiUtilsBundle);
-    const epiUtils = require("epi-utils");
+    const initializeApiHubMappingEngine = ()=>{
+        const gtinResolverBundle = "./../../../gtin-resolver/build/bundles/gtinResolver.js";
+        require(gtinResolverBundle);
+        const epiUtilsBundle = "./../../../epi-utils/build/bundles/epiUtils.js";
+        require(epiUtilsBundle);
+        const epiUtils = require("epi-utils");
 
-    const MessagesPipe = require("epi-utils").getMessagesPipe();
-    const MessageQueuingService = require("epi-utils").loadApi("services").getMessageQueuingServiceInstance();
-    let messagesPipe = new MessagesPipe(10, 2*1000, MessageQueuingService.getNextMessagesBlock);
-
-
-    const ServerDSUStorageImpl = epiUtils.loadApi("services").DSUStorage.getInstance();
-    const mappings = epiUtils.loadApi("mappings")
-
-    const holderInfo = {domain: "epi", subdomain: "default"};
-    let LogService = epiUtils.loadApi("services").LogService;
-
-    const mappingEngine = mappings.getEPIMappingEngine(ServerDSUStorageImpl, {
-        holderInfo: holderInfo,
-        logService: new LogService(ServerDSUStorageImpl)
-    });
-
-    messagesPipe.onNewGroup(async (groupMessages) => {
-        let undigestedMessages = await mappingEngine.digestMessages(groupMessages);
-        console.log("Undigested messages:", undigestedMessages.length)
-    });
+        const MessagesPipe = require("epi-utils").getMessagesPipe();
+        const MessageQueuingService = require("epi-utils").loadApi("services").getMessageQueuingServiceInstance();
+        let messagesPipe = new MessagesPipe(10, 2*1000, MessageQueuingService.getNextMessagesBlock);
 
 
-    server.put("/mappingEngine/:domain", function (request, response, next) {
-        console.log("EPI Mapping Engine called. Not Implemented yet!");
+        const ServerDSUStorageImpl = epiUtils.loadApi("services").DSUStorage.getInstance();
+        const mappings = epiUtils.loadApi("mappings")
 
-        //body contains an array of messages
+        const holderInfo = {domain: "epi", subdomain: "default"};
+        let LogService = epiUtils.loadApi("services").LogService;
 
-        //TODO: Implement DSUStorage and after uncomment the next to lines of code to get access to the Mapping Engine
+        const mappingEngine = mappings.getEPIMappingEngine(ServerDSUStorageImpl, {
+            holderInfo: holderInfo,
+            logService: new LogService(ServerDSUStorageImpl)
+        });
+
+        messagesPipe.onNewGroup(async (groupMessages) => {
+            let undigestedMessages = await mappingEngine.digestMessages(groupMessages);
+            console.log("Undigested messages:", undigestedMessages.length)
+        });
+    }
+
+    server.put("/mappingEngine/:domain",  function  (request, response, next) {
+        console.log("EPI Mapping Engine called. WIP!");
+
+        if(!mappingEngineInitialized){
+            try {
+                initializeApiHubMappingEngine();
+                mappingEngineInitialized = true;
+            }
+            catch (e) {
+                console.error("MappingEngine initialization failed", e);
+                response.statusCode = 500;
+                return response.end();
+            }
+        }
 
         let data = [];
         request.on('data', (chunk) => {
