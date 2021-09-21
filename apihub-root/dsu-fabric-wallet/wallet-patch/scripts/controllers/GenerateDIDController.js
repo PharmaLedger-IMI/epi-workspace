aa85300c4256d639cf60777b4eab2391812e0988
skutner <sava.dumitru.daniel@gmail.com>
2021-09-21 12:40:27 +0300

    added demiurge to workspace

--- apihub-root/dsu-fabric-wallet/wallet-patch/scripts/controllers/GenerateDIDController.js
+++ apihub-root/dsu-fabric-wallet/wallet-patch/scripts/controllers/GenerateDIDController.js
@@ -9,11 +9,28 @@ export default class GenerateDIDController extends ContainerController {
         const openDSU = require("opendsu");
         const w3cDID = openDSU.loadAPI("w3cdid");
         const scAPI = openDSU.loadAPI("sc")
-        setTimeout(async ()=>{
+        setTimeout(async () => {
             const userDetails = await this.getUserDetails();
             const vaultDomain = await $$.promisify(scAPI.getVaultDomain)();
             const identity = await $$.promisify(w3cDID.createIdentity)("name", vaultDomain, userDetails.username);
             this.model.identity = identity.getIdentifier();
+
+            this.on("copy-text", (event) => {
+                console.log(event);
+                const label = element.querySelector("#did").innerHTML;
+                console.log(label)
+                const label1 = element.querySelector("#did").value;
+                console.log(label1)
+                console.log(document);
+            });
+
+            identity.readMessage((err, message) => {
+                // const crypto = require("opendsu").loadAPI("crypto");
+                // const jsonCredential = await promisify(crypto.parseJWTSegments)(model.token);
+                // jsonCredential.signature = $$.Buffer.from(jsonCredential.signature).toString("base64");
+                // model.json = JSON.stringify(jsonCredential, null, 4);
+                history.push("/home");
+            })
         })
     }
 
