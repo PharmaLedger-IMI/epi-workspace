daf883c6f86873a256104126877c853f64d4d50f
skutner <sava.dumitru.daniel@gmail.com>
2021-09-21 12:20:03 +0300

    added demiurge to workspace

---
+++ trust-loader-config/demiurge-wallet/loader/environment.js
@@ -0,0 +1,13 @@
+export default {
+  "appName": "Demiurge",
+  "vault": "server",
+  "agent": "browser",
+  "system":   "any",
+  "browser":  "any",
+  "mode":  "dev-secure",
+  "domain":  "vault",
+  "enclaveType":"WalletDBEnclave",
+  "sw": true,
+  "pwa": false,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
+}
