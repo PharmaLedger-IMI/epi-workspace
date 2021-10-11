b1cd6cb64b41a77739c81577624496ba1c618528
adrian.ganga <adrian.ganga@heavensolutions.com>
2021-10-11 10:21:34 +0300

    Fix assets url for sw = false.

--- trust-loader-config/leaflet-wallet/loader/environment.js
+++ trust-loader-config/leaflet-wallet/loader/environment.js
@@ -8,7 +8,7 @@ export default  {
   "vaultDomain":  "vault",
   "didDomain":  "vault",
   "enclaveType": "WalletDBEnclave",
-  "sw": true,
+  "sw": false,
   "pwa": true,
   "allowPinLogin": false,
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)"
