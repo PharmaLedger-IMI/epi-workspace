69b80ecbe2bb2746a7689f2e569fbb7f3651add6
adrian.ganga <adrian.ganga@heavensolutions.com>
2021-11-12 11:22:48 +0200

    Fix environment settings and rotation screen on android

--- mobile/config/scan-app/android/app/src/main/assets/nodejs-project/apihub-root/app/loader/environment.js
+++ mobile/config/scan-app/android/app/src/main/assets/nodejs-project/apihub-root/app/loader/environment.js
@@ -5,8 +5,11 @@ export default  {
   "system":   "any",
   "browser":  "any",
   "mode":  "mobile-autologin",
-  "domain":  "vault",
+  "vaultDomain":  "vault",
+  "didDomain":  "vault",
+  "enclaveType": "WalletDBEnclave",
   "sw": false,
   "pwa": false,
-  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false)"
+  "allowPinLogin": false,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)"
 }
--- mobile/config/scan-app/ios/PSKNodeServer/PSKNodeServer/nodejsProject/apihub-root/app/loader/environment.js
+++ mobile/config/scan-app/ios/PSKNodeServer/PSKNodeServer/nodejsProject/apihub-root/app/loader/environment.js
@@ -4,9 +4,12 @@ export default  {
   "agent": "browser",
   "system":   "any",
   "browser":  "any",
-  "mode":  "autologin",
-  "domain":  "vault",
+  "mode":  "mobile-autologin",
+  "vaultDomain":  "vault",
+  "didDomain":  "vault",
+  "enclaveType": "WalletDBEnclave",
   "sw": false,
   "pwa": false,
-  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false)"
+  "allowPinLogin": false,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)"
 }
