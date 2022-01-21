0539fd6da9afc6390f7cb9ab4259150fd846cee3
adrian.ganga <adrian.ganga@heavensolutions.com>
2022-01-21 15:59:03 +0200

    #463 Implement 3 options for the wallet login (existing, PIN based, no PIN)

--- trust-loader-config/dsu-fabric-wallet/loader/environment.js
+++ trust-loader-config/dsu-fabric-wallet/loader/environment.js
@@ -14,5 +14,5 @@ export default {
   "pwa": false,
   "allowPinLogin": false,
   "companyName": "Company Inc",
-  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure, sso-direct, sso-pin) sw:(true, false) pwa:(true, false)"
 }
