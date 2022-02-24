14bc345af74085beb74c7e4b4e88f6e955242125
adrian.ganga <adiganga2002@gmail.com>
2022-02-24 09:19:28 +0200

    #505 Automated tests for the creation of products, batches, upload images, add video links

---
+++ .github/push_test_report.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+setup_git() {
+  echo ${GIT_TOKEN}
+  git clone https://${GIT_TOKEN}@github.com/PharmaLedger-IMI/epi-workspace.git ../results > /dev/null 2>&1
+  cd ../results
+  git config user.email "psk.build.track@gmail.com"
+  git config user.name "PSK Build Tracker"
+  git checkout test_reports
+}
+
+commit_test_report() {
+  git pull --all
+  git add -f testReport.html
+  git commit --message "Github Actions build: $GITHUB_RUN_NUMBER"
+}
+
+push_to_github() {
+  git push origin test_reports
+  cd .. && rm -rf results
+}
+
+setup_git
+commit_test_report
+push_to_github
