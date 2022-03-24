c62f47cac84a735984427a76761687b7bc5679b0
skutner <sava.dumitru.daniel@gmail.com>
2022-03-24 12:26:30 +0200

    fixed octopusRun.js script used copySeed octopus command to copy seeds

--- bin/octopusRun.js
+++ bin/octopusRun.js
@@ -2,44 +2,59 @@ const octopus = require("octopus/scripts");
 const octopusActionsRegistry = require('octopus/ActionsRegistry');
 const defaultActionsRegistry = octopusActionsRegistry.getRegistry();
 
-const DEFAULT_PSK_BUNDLES_PATH = "./../privatesky/psknode/bundles";
+const DEFAULT_PSK_BUNDLES_PATH = "./privatesky/psknode/bundles";
 const path = require("path");
 const fs = require("fs");
 
-require(path.join(process.cwd(), DEFAULT_PSK_BUNDLES_PATH));
+require(path.join(process.cwd(), DEFAULT_PSK_BUNDLES_PATH, "openDSU.js"));
 
-const DEFAULT_SEED_PATH = './seed';
-
-const readAlias = (callback) => {
-    const seedPath = path.join(process.cwd(), DEFAULT_SEED_PATH);
-    fs.access(seedPath, (err) => {
+const readAlias = (src, callback) => {
+    fs.access(src, (err) => {
         if (err) {
             octopus.handleError("Seed file does not exist.", err);
             return;
         }
 
-        fs.readFile(seedPath, callback);
+        fs.readFile(src, callback);
     });
 }
 
-const getReadSSIForAlias = (alias, callback) => {
+const getReadSSIForAlias = (aliasSSI, callback) => {
     const openDSU = require("opendsu");
     const keySSISpace = openDSU.loadAPI("keyssi");
-    alias = alias.toString();
-    const aliasSSI = keySSISpace.createAliasSSI(alias);
+    aliasSSI = aliasSSI.toString();
+
     const scAPI = openDSU.loadAPI("sc");
-    scAPI.getSharedEnclave((err, sharedEnclave) => {
+    const dt = openDSU.loadAPI("dt");
+    dt.initialiseBuildWallet((err) => {
         if (err) {
-            octopus.handleError("Failed to get shared enclave", err);
-            return;
+            return callback(err);
         }
 
-        sharedEnclave.getReadForKeySSI(undefined, aliasSSI, callback);
+        scAPI.getSharedEnclave((err, sharedEnclave) => {
+            if (err) {
+                octopus.handleError("Failed to get shared enclave", err);
+                return;
+            }
+
+            sharedEnclave.getReadForKeySSI(aliasSSI, callback);
+        })
     })
 }
 
-const copySeed = (target, callback) => {
-    readAlias((err, alias) => {
+const copySeed = (action, dependency, callback) => {
+    let src = action.src || dependency.src;
+    if (!src) {
+        throw "No source (src) attribute found on: " + JSON.stringify(dependency);
+    }
+
+    if (!action.target) {
+        throw "No target attribute found on: " + JSON.stringify(action);
+    }
+
+    console.log("Start copying " + src + " to folder " + action.target);
+
+    readAlias(src, (err, alias) => {
         if (err) {
             octopus.handleError("Seed file exists, but could not be read.", err);
             return;
@@ -51,7 +66,7 @@ const copySeed = (target, callback) => {
                 return;
             }
 
-            fs.writeFile(target, readSSI, callback);
+            fs.writeFile(action.target, readSSI, callback);
         })
     })
 }
