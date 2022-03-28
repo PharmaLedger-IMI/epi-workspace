c08661fe0fa2ec50b15035a0fb0660a15056cf18
adrian.ganga <adiganga2002@gmail.com>
2022-03-28 13:11:33 +0300

    #592 Create in gtin-resolver a factory of models for batches and products + clean-up + moved servicies in gtin-resolver

--- trust-loader-config/leaflet-wallet/loader/environment.js
+++ trust-loader-config/leaflet-wallet/loader/environment.js
@@ -11,5 +11,8 @@ export default  {
   "sw": false,
   "pwa": false,
   "allowPinLogin": false,
-  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)"
+  "disabledFeatures": "",
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)"+
+    "disabledFeatures: '01, 02 ...' - (01 = Patient leaflet, 02 = Show leaflet if batch expired, 03 = Show leaflet if batch unknown, 04 = Healthcare practitioner info, 05= Video source, 06 = Adverse Events reporting" +
+    "07 = Anti-counterfeiting functions, 08 = Recall functions, 09 = Batch message) "
 }
