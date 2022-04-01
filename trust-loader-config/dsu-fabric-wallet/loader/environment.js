b93d26523bdec22de5d54d55874d371e040bf152
adrian.ganga <adiganga2002@gmail.com>
2022-04-01 13:36:51 +0300

    #650 Decision on ePI metadata versioning

--- trust-loader-config/dsu-fabric-wallet/loader/environment.js
+++ trust-loader-config/dsu-fabric-wallet/loader/environment.js
@@ -15,6 +15,7 @@ export default {
   "allowPinLogin": false,
   "companyName": "Company Inc",
   "disabledFeatures": "",
+  "epiProtocolVersion": 1,
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any)" +
     " mode:(autologin,dev-autologin, secure, dev-secure, sso-direct, sso-pin) sw:(true, false) pwa:(true, false)" +
     "disabledFeatures: '01, 02 ...' - (01 = Patient leaflet, 02 = Show leaflet if batch expired, 03 = Show leaflet if batch unknown, 04 = Healthcare practitioner info, 05= Video source, 06 = Adverse Events reporting" +
