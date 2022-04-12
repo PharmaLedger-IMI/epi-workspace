671bc9a7c87ce28f44ecdfc209698e7bd51a67ac
adrian.ganga <adiganga2002@gmail.com>
2022-04-12 11:05:14 +0300

    #671 LockFeatures flag in environment

--- trust-loader-config/dsu-fabric-wallet/loader/environment.js
+++ trust-loader-config/dsu-fabric-wallet/loader/environment.js
@@ -2,22 +2,23 @@ export default {
   "appName": "DSU_Fabric",
   "vault": "server",
   "agent": "browser",
-  "system":   "any",
-  "browser":  "any",
-  "mode":  "dev-secure",
-  "vaultDomain":  "vault",
-  "didDomain":  "vault",
-  "epiDomain":  "epi",
-  "epiSubdomain":  "default",
+  "system": "any",
+  "browser": "any",
+  "mode": "dev-secure",
+  "vaultDomain": "vault",
+  "didDomain": "vault",
+  "epiDomain": "epi",
+  "epiSubdomain": "default",
   "enclaveType": "WalletDBEnclave",
   "sw": false,
   "pwa": false,
   "allowPinLogin": false,
   "companyName": "Company Inc",
   "disabledFeatures": "",
+  "lockFeatures": false,
   "epiProtocolVersion": 1,
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any)" +
-    " mode:(autologin,dev-autologin, secure, dev-secure, sso-direct, sso-pin) sw:(true, false) pwa:(true, false)" +
+    " mode:(autologin,dev-autologin, secure, dev-secure, sso-direct, sso-pin) sw:(true, false) pwa:(true, false) lockFeatures: (true, false)" +
     "disabledFeatures: '01, 02 ...' - (01 = Patient leaflet, 02 = Show leaflet if batch expired, 03 = Show leaflet if batch unknown, 04 = Healthcare practitioner info, 05= Video source, 06 = Adverse Events reporting" +
     "07 = Anti-counterfeiting functions, 08 = Recall functions, 09 = Batch message) "
 }
