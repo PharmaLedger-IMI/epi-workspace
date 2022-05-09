969f6b5a8ef94013ba83c14a50ece814c17c9abd
adrian.ganga <adiganga2002@gmail.com>
2022-05-09 10:37:24 +0300

    #702 Adding new group in the Demiurge seems to not working

--- trust-loader-config/demiurge-wallet/loader/environment.js
+++ trust-loader-config/demiurge-wallet/loader/environment.js
@@ -8,6 +8,7 @@ export default {
   "vaultDomain":  "vault",
   "didDomain":  "vault",
   "enclaveType":"WalletDBEnclave",
+  "disabledFeatures": "add-group",
   "sw": false,
   "pwa": false,
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
