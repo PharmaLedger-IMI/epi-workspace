12ca6d48ea32bbe1e1b120884e7f1b217609223d
IBogdan4226 <bogdan-marian.ionita@student.tuiasi.ro>
2022-07-08 10:38:20 +0300

    Refactorization of the apihub landing page

--- apihub-root/assets/defaultValues.js
+++ apihub-root/assets/defaultValues.js
@@ -1,7 +1,7 @@
 export default 
 {
     "background":["/assets/images/background1.png","/assets/images/background2.png","/assets/images/background3.png",
-    "/assets/images/background4.png","/assets/images/background5.png","/assets/images/background6.png"],
+    "/assets/images/background4.png"],
     "default_item":{
         "href":["#"],
         "background_color":["#FAA368","#D8BAFF","#33B9D4","#A1DDC2","#BCE4D2","#ECEEFF"],
