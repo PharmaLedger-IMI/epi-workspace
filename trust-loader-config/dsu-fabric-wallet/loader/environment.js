e47dda67fd0a98a7a3d0bf2ba0e9badf90e17d10
adrian.ganga <adiganga2002@gmail.com>
2022-09-22 17:39:15 +0300

    Fixes for android mobile app

--- trust-loader-config/dsu-fabric-wallet/loader/environment.js
+++ trust-loader-config/dsu-fabric-wallet/loader/environment.js
@@ -17,6 +17,7 @@ export default {
   "disabledFeatures": "04",
   "lockFeatures": true,
   "epiProtocolVersion": 1,
+  "appBuildVersion": "v1.5.4",
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any)" +
     " mode:(autologin,dev-autologin, secure, dev-secure, sso-direct, sso-pin) sw:(true, false) pwa:(true, false) lockFeatures: (true, false)" +
     "disabledFeatures: '01, 02 ...' - (01 = Patient leaflet, 02 = Show leaflet if batch expired, 03 = Show leaflet if batch unknown, 04 = Healthcare practitioner info, 05= Video source, 06 = Adverse Events reporting" +
--- trust-loader-config/leaflet-wallet/loader/environment.js
+++ trust-loader-config/leaflet-wallet/loader/environment.js
@@ -15,6 +15,7 @@ export default  {
   "disabledFeatures": "04",
   "allowAdvancedSettings": true,
   "epiProtocolVersion": 1,
+  "appBuildVersion": "v1.5.4",
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false) lockFeatures: (true, false)"+
     "disabledFeatures: '01, 02 ...' - (01 = Patient leaflet, 02 = Show leaflet if batch expired, 03 = Show leaflet if batch unknown, 04 = Healthcare practitioner info, 05= Video source, 06 = Adverse Events reporting" +
     "07 = Anti-counterfeiting functions, 08 = Recall functions, 09 = Batch message) allowAdvancedSettings:(true, false)"
