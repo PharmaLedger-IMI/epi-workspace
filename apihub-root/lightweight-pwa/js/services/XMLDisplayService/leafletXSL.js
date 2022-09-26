534d853188de65e1d2a8598b803602e2ec2b05df
adrian.ganga <adiganga2002@gmail.com>
2022-09-26 14:53:12 +0300

    Fixes for jailbreak error, language missing and ignored form ui elements for leaflet display

--- apihub-root/lightweight-pwa/js/services/XMLDisplayService/leafletXSL.js
+++ apihub-root/lightweight-pwa/js/services/XMLDisplayService/leafletXSL.js
@@ -175,7 +175,11 @@ const defualtXslContent = `<?xml version="1.0" encoding="UTF-8"?>
 </xsl:stylesheet>`
 
 const acordisXslContent = `<?xml version="1.0" encoding="UTF-8"?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:xs="urn:hl7-org:v3"
+                xmlns="urn:hl7-org:v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="urn:hl7-org:v3 https://www.accessdata.fda.gov/spl/schema/spl.xsd">
 
     <xsl:output method="html" version="1.0"
                 encoding="UTF-8" indent="yes" doctype-public="-//W3C//DTD XHTML 1.1//EN"/>
@@ -190,85 +194,10 @@ const acordisXslContent = `<?xml version="1.0" encoding="UTF-8"?>
     <xsl:template match="document">
             <div><xsl:apply-templates select="@*|node()"/></div>
     </xsl:template>
-
-    <xsl:template match="document/p[@class='Type']">
-        <div style="display:none;" class="ignored_from_ui">
-            <xsl:apply-templates select="node()">
-            </xsl:apply-templates>
-        </div>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Product_Name']">
-        <h1 style="display:none;" class="ignored_from_ui">
-            <xsl:apply-templates select="node()">
-            </xsl:apply-templates>
-        </h1>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Ingredient Substance']">
-        <p class="ingredient_substance ignored_from_ui" style="display:none;">
-            <span>
-                <b><xsl:value-of select="@class"/>:</b>
-            </span>
-            <span>
-                <xsl:apply-templates select="@*|node()"></xsl:apply-templates>
-            </span>
-        </p>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Read Instructions'][1]">
-        <xsl:text disable-output-escaping='yes'>&lt;div class="read_instructions" style="display:none;" class="ignored_from_ui" &gt;</xsl:text>
-        <p style="display:none;" class="ignored_from_ui"><xsl:apply-templates select="node()" /></p>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Read Instructions'][position()>1 and position()&lt;last()]">
-        <p style="display:none;" class="ignored_from_ui"><xsl:apply-templates select="node()" /></p>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Read Instructions'][last()]">
-        <p style="display:none;" class="ignored_from_ui"><xsl:apply-templates select="node()" /></p>
-        <xsl:text disable-output-escaping='yes'>&lt;/div&gt;</xsl:text>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Table of Content'][1]">
-        <xsl:text disable-output-escaping='yes'>&lt;div class="table_of_content ignored_from_ui" style="display:none;" &gt;</xsl:text>
-        <h2 style="display:none;" class="ignored_from_ui"><xsl:apply-templates select="node()" /></h2>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Table of Content'][position()>1 and position()&lt;last()]">
-        <p style="display:none;" class="ignored_from_ui"><xsl:apply-templates select="node()" /></p>
-    </xsl:template>
-
-    <xsl:template match="document/p[@class='Table of Content'][last()]">
-        <p style="display:none;" class="ignored_from_ui"><xsl:apply-templates select="node()" /></p>
-        <xsl:text disable-output-escaping='yes'>&lt;/div&gt;</xsl:text>
-    </xsl:template>
-    
-        <xsl:template match="document/section">
-        <div class="section leaflet-accordion-item">
-            <xsl:apply-templates select="header"/>
-                <div class="leaflet-accordion-item-content">
-                     <xsl:apply-templates select="*[not(self::header)]"/>
-                </div>
-        </div>
-    </xsl:template>
-    
-    
-    <xsl:template match="document/section/header">
-        <h5>
-            <xsl:apply-templates select="node()" />
-        </h5>
-    </xsl:template>
-
-    <xsl:template match="document/section/p">
-        <p><xsl:apply-templates select="node()" /></p>
-    </xsl:template>
-    
     
     <xsl:param name="quote">"</xsl:param>
     <xsl:param name="space">\\0020</xsl:param>
     <xsl:template match="//ul">
-
         <ul>
           <xsl:attribute name="style"> list-style-type: <xsl:value-of select="$quote"/><xsl:value-of select="@data-type"/><xsl:value-of select="$space"/><xsl:value-of select="$quote"/>;
           </xsl:attribute>
@@ -290,20 +219,28 @@ const acordisXslContent = `<?xml version="1.0" encoding="UTF-8"?>
     <xsl:template match="//section//p">
         <p><xsl:apply-templates select="node()" /></p>
     </xsl:template>
+    
+    <xsl:template match="//figure">
+        <figure><xsl:apply-templates select="node()" /></figure>
+    </xsl:template>
 
-       <xsl:template match="//figure">
-        <figure class="selective">
-              <style>
-                .selective :not(img){
-                display:none;
-                }
-              </style>
-            <xsl:apply-templates select="node()" />
-        </figure>
-       </xsl:template>
-
-   
-
+    <xsl:template match="//figure/*[not(self::img)]">
+        <section style="display:none;" class="ignore_from_ui"><xsl:apply-templates select="node()" /></section>
+    </xsl:template>
+    
+    
+    <xsl:template match="//figure/img">
+        <xsl:variable name="_src">
+            <xsl:value-of select="@src"/>
+        </xsl:variable>
+        <img>
+            <xsl:attribute name="src">
+                <xsl:value-of select="concat($resources_path, $_src)"/>
+            </xsl:attribute>
+            <xsl:apply-templates select="node()"/>
+        </img>
+    </xsl:template>
+    
     <xsl:template match="//table">
         <table><xsl:apply-templates select="node()" /></table>
     </xsl:template>
@@ -311,7 +248,39 @@ const acordisXslContent = `<?xml version="1.0" encoding="UTF-8"?>
     <xsl:template match="//tr">
         <tr><xsl:apply-templates select="node()" /></tr>
     </xsl:template>
-
+      
+     <xsl:template match="//*[@class='Table of Content']" priority="9">
+        <div style="display:none;" class="leaflet_hidden_section ignore_from_ui"><xsl:apply-templates select="@class|node()"/></div>
+    </xsl:template>
+    
+      <xsl:template match="//*[@class='Type']" priority="9">
+        <div style="display:none;" class="leaflet_hidden_section ignore_from_ui"><xsl:apply-templates select="@class|node()"/></div>
+    </xsl:template>
+   
+    <xsl:template match="//*[@class='Product_Name']" priority="9">
+        <div style="display:none;" class="leaflet_hidden_section ignore_from_ui"><xsl:apply-templates select="@class|node()"/></div>
+    </xsl:template>
+   <xsl:template match="//*[@class='Ingredient Substance']" priority="9">
+        <div style="display:none;" class="leaflet_hidden_section ignore_from_ui"><xsl:apply-templates select="@class|node()"/></div>
+    </xsl:template>
+   <xsl:template match="//*[@class='Read Instructions']" priority="9">
+        <div style="display:none;" class="leaflet_hidden_section ignore_from_ui"><xsl:apply-templates select="@class|node()"/></div>
+    </xsl:template>
+    
+    <xsl:template match="document/section">
+        <div class="section leaflet-accordion-item">
+            <xsl:apply-templates select="header"/>
+                <div class="leaflet-accordion-item-content">
+                     <xsl:apply-templates select="*[not(self::header)]"/>
+                </div>
+        </div>
+    </xsl:template>
+    
+    <xsl:template match="document/section/header">
+        <h5>
+            <xsl:apply-templates select="node()" />
+        </h5>
+    </xsl:template>
 </xsl:stylesheet>`;
 
 export {
