6dab7c50bf5f543363de2493a4f4415cdf965c7b
adrian.ganga <adiganga2002@gmail.com>
2022-10-31 11:14:44 +0200

    update release version

--- apihub-root/lightweight-pwa/environment.js
+++ apihub-root/lightweight-pwa/environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "localhost.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "1.5.4",
+  "appBuildVersion": "1.6.0",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
