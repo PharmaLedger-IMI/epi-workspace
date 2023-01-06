731c7e2f75038d74c597f616423a59e927f842ec
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-01-06 16:35:08 +0200

    new script to obtain zip files for static analysis

--- .github/generateZipArchivesForStaticAnalysis.sh
+++ .github/generateZipArchivesForStaticAnalysis.sh
@@ -2,21 +2,21 @@ currentDate=`date +'%m-%d-%Y'`
 initialPWD=`pwd`
 
 cd ../opendsu-sdk/modules/apihub
-zip -r "$initialPWD/Backend-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+zip -r "$initialPWD/Backend-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "package-lock.json"
 cd $initialPWD
 
 cd ../gtin-resolver
-zip -r "$initialPWD/GtinResolver-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+zip -r "$initialPWD/GtinResolver-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "package-lock.json"
 cd $initialPWD
 
 cd ../apihub-root/lightweight-pwa
-zip -r "$initialPWD/LPWA-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+zip -r "$initialPWD/LPWA-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "package-lock.json"
 cd $initialPWD
 
 cd ../demiurge
-zip -r "$initialPWD/Demiurge-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "seed"
+zip -r "$initialPWD/Demiurge-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "seed" "package-lock.json"
 cd $initialPWD
 
 cd ../dsu-fabric-ssapp
-zip -r "$initialPWD/EnterpriseWallet-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "seed"
+zip -r "$initialPWD/EnterpriseWallet-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "seed" "package-lock.jsonn"
 cd $initialPWD
