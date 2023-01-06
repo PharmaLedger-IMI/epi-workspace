edaa12a71e2e24b61f20c568db08af1dbd95ade4
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-01-06 16:29:34 +0200

    new script to obtain zip files for static analysis

--- .github/generateZipArchivesForStaticAnalysis.sh
+++ .github/generateZipArchivesForStaticAnalysis.sh
@@ -3,15 +3,20 @@ initialPWD=`pwd`
 
 cd ../opendsu-sdk/modules/apihub
 zip -r "$initialPWD/Backend-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+cd $initialPWD
 
 cd ../gtin-resolver
 zip -r "$initialPWD/GtinResolver-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+cd $initialPWD
 
 cd ../apihub-root/lightweight-pwa
 zip -r "$initialPWD/LPWA-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+cd $initialPWD
 
 cd ../demiurge
-zip -r "$initialPWD/Demiurge-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+zip -r "$initialPWD/Demiurge-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "seed"
+cd $initialPWD
 
 cd ../dsu-fabric-ssapp
-zip -r "$initialPWD/EnterpriseWallet-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**"
+zip -r "$initialPWD/EnterpriseWallet-$currentDate.zip" . -x "**/.git/**" "**/node_modules/**" "seed"
+cd $initialPWD
