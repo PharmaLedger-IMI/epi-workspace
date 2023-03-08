4335443e2abb5a27368e4bb931e036b4d6027dd6
Alboaie Sînică <salboaie@gmail.com>
2023-03-08 13:43:30 +0200

    Version update

--- apihub-root/lightweight-pwa/environment.js
+++ apihub-root/lightweight-pwa/environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "localhost.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.0.2.a",
+  "appBuildVersion": "2023.0.4",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
