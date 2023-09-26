16af333cd2c3acfe9dc1a05fe6fbb096533c88fd
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-09-26 16:28:58 +0300

    sso small bug fix

--- apihub-root/SessionMonitor.js
+++ apihub-root/SessionMonitor.js
@@ -19,11 +19,15 @@ function parseCookies(cookies) {
 }
 
 const handler = setInterval(() => {
-    const {sessionExpiryTime, logout} = parseCookies(document.cookie);
+    let {sessionExpiryTime, logout} = parseCookies(document.cookie);
+
     if(logout === "true"){
+        document.body.setAttribute("style", "visibility:hidden");
         alert("A Single Sign-On (SSO) login process is detected to be in progress across multiple browser instances. If this assessment is incorrect, kindly terminate all browser instances and recommence the login procedure.");
+        logout = parseCookies(document.cookie).logout;
         return;
     }
+    document.body.setAttribute("style", "visibility:visible");
     if (sessionExpiryTime && parseInt(sessionExpiryTime) < Date.now()) {
         clearInterval(handler);
         window.disableRefreshSafetyAlert = true;
